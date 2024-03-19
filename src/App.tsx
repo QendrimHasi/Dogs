2ee59f9fcf8550f6617179fe82c6d0931d481c31
QendrimHasi <qendrim.hasi@hotmail.com>
2024-03-19 20:00:03 +0100

    init mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import "./App.css";
 import Routes from "./routes";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+const theme = createTheme();
 
 function App() {
-  return <Routes />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Routes />
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Button } from "@mui/material";
 
 function Home() {
-  return <div>Home page</div>;
+  return (
+    <div>
+      Home page{" "}
+      <Button color="primary" variant="contained">
+        test
+      </Button>
+    </div>
+  );
 }
 
 export default Home;
